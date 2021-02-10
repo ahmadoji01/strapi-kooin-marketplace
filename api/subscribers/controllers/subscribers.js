be574196c0c3412a60788247f8a28c43b8344923
Ahmad Ridwan Fauzi <ahmadrfauzi94@gmail.com>
2021-02-10 16:15:58 +0700

    feature: Added Custom Create Endpoint to Check if Email Exists

--- api/subscribers/controllers/subscribers.js
+++ api/subscribers/controllers/subscribers.js
@@ -1,8 +1,25 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        const { email } = ctx.request.body;
+
+        let entity = await strapi.services.subscribers.findOne({ email: email });
+        if (entity)
+            return ctx.throw(422, 'Email already exists');
+        
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.subscribers.create(data, { files });
+        } else {
+          entity = await strapi.services.subscribers.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.subscribers });
+    },
+};
