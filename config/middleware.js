65e5607b16aa8884a06d041652ce887f9cda47b2
Ahmad Ridwan Fauzi <ahmadrfauzi94@gmail.com>
2021-02-16 11:58:42 +0700

    build: Edited Middleware Config

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     settings: {
       cors: {
-        origin: ['http://localhost:3000', 'http://localhost:1337', 'http://149.28.154.95:1337', 'https://mysite.com', 'https://www.mysite.com', 'https://kooin.id', 'http://45.76.159.144:3000/'],
+        origin: ['http://localhost:3000', 'http://localhost:1337', 'https://mysite.com', 'https://www.mysite.com'],
         headers: "*",
       },
     },
