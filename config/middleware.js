de3cf58ca3238370f57cf68e2e84161481cd594e
Ahmad Ridwan Fauzi <ahmadrfauzi94@gmail.com>
2021-02-20 21:24:32 +0700

    feature: Added Rich Text Editor for Rich Text Type FIeld

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     settings: {
       cors: {
-        origin: ['http://localhost:3000', 'http://localhost:1337', 'https://mysite.com', 'https://www.mysite.com'],
+        origin: ['http://localhost:3000', 'http://localhost:1337', 'https://mysite.com', 'http://localhost:8000', 'https://www.mysite.com'],
         headers: "*",
       },
     },
