d9ebf52e174a520fffe678696ac0f6d8a81e7bbe
Ahmad Ridwan Fauzi <ahmadrfauzi94@gmail.com>
2021-02-22 10:48:19 +0700

    feature: Added Search Products Endpoint

--- api/products/controllers/products.js
+++ api/products/controllers/products.js
@@ -20,5 +20,33 @@ module.exports = {
 
         let entity = await strapi.query('products').find({ "category.name": product.category.name, _limit: 5 });
         return sanitizeEntity(entity, { model: strapi.models.products });
+    },
+
+    async search(ctx) {
+        const { category, q, sort, priceLTE, priceGTE } = ctx.query;
+
+        let entity;
+        let query = {};
+        if (sort) {
+            query = Object.assign(query, {_sort: sort});
+        }
+        if (category) {
+            query = Object.assign(query, {"category.name": category});
+        }
+        if (priceLTE) {
+            query = Object.assign(query, {price_lte: priceLTE});
+        }
+        if (priceGTE) {
+            query = Object.assign(query, {price_gte: priceGTE});
+        }
+        
+        if (q) {
+            query = Object.assign(query, {_q: q})
+            entity = await strapi.query('products').search(query);
+        } else {
+            entity = await strapi.query('products').find(query)
+        }
+
+        return sanitizeEntity(entity, { model: strapi.models.products });
     }
 };
