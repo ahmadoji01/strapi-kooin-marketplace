ab96bc86ec93e889c9d750e91f65eead56d50d86
Ahmad Ridwan Fauzi <ahmadrfauzi94@gmail.com>
2021-03-11 23:23:45 +0700

    fix: Address Assignment Error

--- api/addresses/controllers/addresses.js
+++ api/addresses/controllers/addresses.js
@@ -14,5 +14,24 @@ module.exports = {
         let entity = await strapi.query('addresses').findOne({ "users_permissions_user.id": id });
 
         return sanitizeEntity(entity, {model: strapi.models.addresses });
+    },
+
+    async updateMyAddress(ctx) {
+
+        const { id } = ctx.state.user;
+        let updateData = {
+            ...ctx.request.body,
+            'users_permissions_user':{ id },
+        };
+
+        let userAddress = await strapi.query('addresses').findOne({ "users_permissions_user.id": id });
+        if (!userAddress) {
+            userAddress = await strapi.query('addresses').create(updateData);
+            return sanitizeEntity(userAddress, {model: strapi.models.addresses });
+        }
+        
+        const addressId = userAddress.id;
+        const entity = await strapi.query('addresses').update({ "id": addressId }, updateData);
+        return sanitizeEntity(entity, {model: strapi.models.addresses });
     }
 };
