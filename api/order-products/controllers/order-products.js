0f4973ff827f9776bc9796564f7caf29937e207d
Ahmad Ridwan Fauzi <ahmadrfauzi94@gmail.com>
2021-03-19 19:04:08 +0700

    feature: Add Insert Order Product into Order

--- api/order-products/controllers/order-products.js
+++ api/order-products/controllers/order-products.js
@@ -5,4 +5,27 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async insertProductIntoOrder(ctx) {
+        const user = ctx.state.user;
+
+        let order = await strapi.services.orders.findOne({ "users_permissions_user.id": user.id, status: "active" });
+        if (!order) {
+            order = await strapi.services.orders.create({ users_permissions_user: user, status: "active" });
+        }
+
+        let orderProductData = {
+            ...ctx.request.body,
+            order,
+        };
+
+        let orderProduct = await strapi.services.order-products.findOne({ "order.id": order.id, "product.id": "productData.product.id" });
+        let result;
+        if (orderProduct) {
+            result = await strapi.services.order-products.edit({ id: orderProduct.id }, orderProductData);
+            return sanitizeEntity(result, { model: strapi.models.order-products });
+        }
+        result = await strapi.services.order-products.create(orderProductData);
+        return sanitizeEntity(result, { model: strapi.models.order-products });
+    },
+};
